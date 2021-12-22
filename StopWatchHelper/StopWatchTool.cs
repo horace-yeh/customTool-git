f0ed36d9023b1f4ce7a48f5e6e95b974bf8d0997
horace.yeh <horaceyeh.web@gmail.com>
2021-12-22 22:48:22 +0800

    add stopwatch extend

---
+++ StopWatchHelper/StopWatchTool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatchHelper
+{
+    public class StopWatchTool
+    {
+        #region Example
+
+        //public void Test()
+        //{
+        //    Benchmark(() =>
+        //    {
+        //        Console.WriteLine("test");
+        //    }, "test");
+        //}
+
+        #endregion Example
+
+        public static void Benchmark(Action act,string label)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            act();
+            sw.Stop();
+            Console.WriteLine($"{label} : {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
