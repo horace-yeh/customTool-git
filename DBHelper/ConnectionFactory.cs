c2def7f0de4add37c8fe45393e4a89c5d3218430
horace.yeh <horaceyeh.web@gmail.com>
2022-05-28 23:53:29 +0800

    Add idbconnevtion dapper extension

---
+++ DBHelper/ConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection CreateConnection(string connName = "MainConnectionString")
+        {
+            switch (connName)
+            {
+                case "MainConnectionString":
+                    var conn = ConfigurationManager.ConnectionStrings[connName].ConnectionString;
+                    return new SqlConnection(conn);
+                default:
+                    throw new ArgumentException("Not Found ConnectionName");
+            }
+        }
+    }
+}
---
+++ DBHelper/DapperIDbConnAsyncTool.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DBHelper
+{
+    public class DapperIDbConnAsyncTool
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        public DapperIDbConnAsyncTool()
+        {
+            this._connectionFactory = new ConnectionFactory();
+        }
+
+        public async Task<int> DapperNonQueryAsync(string sql, object pms)
+        {
+            using (var conn = this._connectionFactory.CreateConnection())
+            {
+                return await conn.ExecuteAsync(sql, pms);
+            }
+        }
+
+        public async Task<int> DapperNonQuerySPAsync(string sql, object pms)
+        {
+            using (var conn = this._connectionFactory.CreateConnection())
+            {
+                return await conn.ExecuteAsync(sql, pms, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<IEnumerable<T>> DapperQueryAsync<T>(string sql, object pms, int? timeout = null)
+        {
+
+            using (var conn = this._connectionFactory.CreateConnection())
+            {
+                return await conn.QueryAsync<T>(sql, pms, commandTimeout: timeout);
+            }
+        }
+
+        public async Task<IEnumerable<T>> DapperQuerySPAsync<T>(string sql, object pms, int? timeout = null)
+        {
+            using (var conn = this._connectionFactory.CreateConnection())
+            {
+                return await conn.QueryAsync<T>(sql, pms, commandType: CommandType.StoredProcedure, commandTimeout: timeout);
+            }
+        }
+
+        #region 設定Dapper參數
+
+        //參數型態的定義可以有效提升效能
+        //Ref: https://dotblogs.com.tw/supershowwei/2019/08/12/232213
+
+        /// <summary>
+        ///     Length of the string is default 4000
+        /// </summary>
+        public DbString ToChar(string me)
+        {
+            return new DbString { Value = me, IsAnsi = true, IsFixedLength = true };
+        }
+
+        /// <summary>
+        ///     Length of the string -1 for max
+        /// </summary>
+        public DbString ToChar(string me, int length)
+        {
+            return new DbString { Value = me, Length = length, IsAnsi = true, IsFixedLength = true };
+        }
+
+        /// <summary>
+        ///     Length of the string is default 4000
+        /// </summary>
+        public DbString ToNChar(string me)
+        {
+            return new DbString { Value = me, IsFixedLength = true };
+        }
+
+        /// <summary>
+        ///     Length of the string -1 for max
+        /// </summary>
+        public DbString ToNChar(string me, int length)
+        {
+            return new DbString { Value = me, Length = length, IsFixedLength = true };
+        }
+
+        /// <summary>
+        ///     Length of the string is default 4000
+        /// </summary>
+        public DbString ToNVarchar(string me)
+        {
+            return new DbString { Value = me };
+        }
+
+        /// <summary>
+        ///     Length of the string -1 for max
+        /// </summary>
+        public DbString ToNVarchar(string me, int length)
+        {
+            return new DbString { Value = me, Length = length };
+        }
+
+        /// <summary>
+        ///     Length of the string is default 4000
+        /// </summary>
+        public DbString ToVarchar(string me)
+        {
+            return new DbString { Value = me, IsAnsi = true };
+        }
+
+        /// <summary>
+        ///     Length of the string -1 for max
+        /// </summary>
+        public DbString ToVarchar(string me, int length)
+        {
+            return new DbString { Value = me, Length = length, IsAnsi = true };
+        }
+
+        #endregion 設定Dapper參數
+    }
+}
